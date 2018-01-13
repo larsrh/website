49f720e593f29f01f2f64f57fc68ce1adbde5e98
Lars Hupel <lars.hupel@mytum.de>
2018-01-13 17:05:46 +0100

    run site through html5validator

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-bundle install
 bundle exec jekyll build
 
 bundle exec htmlproofer --checks-to-ignore LinkCheck --assume-extension --disable-external --check-html ./_site
+
+virtualenv venv
+source ./venv/bin/activate
+pip install html5validator
+html5validator --root ./_site --ignore 'Illegal character in fragment' google
