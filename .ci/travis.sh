2dd6f64aae9e6f2a88721a09a15fe96313f7eb93
Lars Hupel <lars.hupel@mytum.de>
2020-05-25 13:42:08 +0200

    bump dependencies

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -9,4 +9,4 @@ python3 -m venv venv
 source ./venv/bin/activate
 pip3 install -r .ci/requirements.txt
 html5validator --root ./_site \
-  --ignore 'Illegal character in fragment'
+  --ignore 'Illegal character in fragment' 'endnote'
