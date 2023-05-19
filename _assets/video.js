92a72811f2279713c310103c90f759a414e74b89
Lars Hupel <lars.hupel@mytum.de>
2023-05-19 10:15:22 +0200

    skip if no consent

--- _assets/video.js
+++ _assets/video.js
@@ -1,5 +1,7 @@
 for (const container of document.querySelectorAll("div.video")) {
   const consent = container.querySelector("div.video-consent");
+  if (!consent) continue;
+
   const template = container.querySelector("template");
   container.querySelector("button").onclick = () => {
     container.replaceChildren(template.content.cloneNode(true));
