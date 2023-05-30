767c8a67eda8e8dbc4aa7478a10803a80ba7c465
Lars Hupel <lars.hupel@mytum.de>
2023-05-30 14:02:54 +0200

    use pdftoppm to convert PDFs
    ImageMagick kept creating strange colour artifacts. Now, use a two-step
    pipeline, which also improves performance.

--- scripts/slides.sh
+++ scripts/slides.sh
@@ -6,11 +6,29 @@ file="$1"
 short="$(basename "$file" ".pdf")"
 dir="$(dirname "$file")"
 
+echo "*** converting to png first ***"
+
+temp="$(mktemp -d)"
+
+cleanup()
+{
+  echo "*** cleaning up ***"
+  rm -rf "$temp"
+}
+
+trap cleanup EXIT
+
+pdftoppm -png -scale-to 1280 "$file" "$temp/convert"
+
 make_images()
 {
   local ext="$1"
   echo "*** converting to $ext ***"
-  convert -density 300 "$file" +adjoin -resize 1280x720 "$dir/$short.$ext"
+  declare -i count=0
+  for file in "$temp/convert-"*; do
+    magick -density 300 "$file" "$dir/$short-$count.$ext"
+    count=$(( count + 1 ))
+  done
 }
 
 make_images webp
